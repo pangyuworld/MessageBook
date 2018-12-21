e65a25a11354f83bf624aea68bd3dbe646cd84d3
pangyuworld <a904237539@qq.com>
2018-12-21 16:13:22 +0800

    项目介绍+Spring Boot基础框架

---
+++ src/main/java/com/pang/messagebook/MessageBookApplication.java
@@ -0,0 +1,14 @@
+package com.pang.messagebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBookApplication.class, args);
+    }
+
+}
+
